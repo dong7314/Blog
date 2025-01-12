fb22246b2cd39d646b92ca6da70eedbe9203fc9a
DongYeopLee <103983410+dong7314@users.noreply.github.com>
2025-01-12 21:36:44 +0900

    [Backend]: (add) 팔로수 guard 추가

--- apps/backend/blog/src/follow/follow.controller.ts
+++ apps/backend/blog/src/follow/follow.controller.ts
@@ -1,23 +1,55 @@
-import { Controller, Post, Delete, Param, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Delete,
+  Param,
+  Get,
+  UseGuards,
+  Req,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+import { JwtService } from '@nestjs/jwt';
 import { FollowService } from './follow.service';
+import { AuthGuard } from 'src/guard/auth.guard';
 
 @Controller('api.follow')
 export class FollowController {
-  constructor(private readonly followService: FollowService) {}
+  constructor(
+    private readonly followService: FollowService,
+    private jwtService: JwtService,
+  ) {}
 
   @Post(':followerId/:followingId')
+  @UseGuards(AuthGuard)
   async followUser(
     @Param('followerId') followerId: number,
     @Param('followingId') followingId: number,
+    @Req() req,
   ): Promise<void> {
+    const userId = this.getUserId(req);
+
+    if (userId !== followerId) {
+      throw new UnauthorizedException('권한이 없는 유저입니다.');
+    }
+
     return this.followService.followUser(followerId, followingId);
   }
 
   @Delete(':followerId/:followingId')
+  @UseGuards(AuthGuard)
   async unfollowUser(
     @Param('followerId') followerId: number,
     @Param('followingId') followingId: number,
+    @Req() req,
   ): Promise<void> {
+    const userId = this.getUserId(req);
+
+    if (userId !== followerId) {
+      throw new UnauthorizedException('권한이 없는 유저입니다.');
+    }
+
     return this.followService.unfollowUser(followerId, followingId);
   }
 
@@ -29,5 +61,16 @@ export class FollowController {
   @Get('following/:userId')
   async getFollowing(@Param('userId') userId: number) {
     return this.followService.getFollowing(userId);
+  }
+
+  getUserId(req: Request) {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = this.jwtService.decode(token) as { id: number };
+
+    if (!decoded || !decoded.id) {
+      throw new Error('토큰이 유효하지 않습니다.');
+    }
+
+    return decoded.id;
   }
 }
--- apps/backend/blog/src/follow/follow.module.ts
+++ apps/backend/blog/src/follow/follow.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Follow } from './entity/follow.entity';
+import { JwtService } from '@nestjs/jwt';
 import { UserEntity } from 'src/users/entity/user.entity';
 import { FollowService } from './follow.service';
 import { FollowController } from './follow.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Follow, UserEntity])],
-  providers: [FollowService],
+  providers: [FollowService, JwtService],
   controllers: [FollowController],
 })
 export class FollowModule {}
