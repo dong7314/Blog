5023ff830651f04982eea5685f68a31800cc5f2d
DongYeopL <eaea7314@gmail.com>
2025-01-16 17:24:34 +0900

    [Backend]: (add) cors filter 추가

--- apps/backend/blog/src/main.ts
+++ apps/backend/blog/src/main.ts
@@ -6,6 +6,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // cors
+  const corsOrigin =
+    process.env.NODE_ENV === 'production'
+      ? 'https://www.example.com'
+      : 'http://localhost:3000';
+  app.enableCors({
+    origin: corsOrigin,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   // cookie parser
   app.use(cookieParser());
   // swagger 세팅 진행
