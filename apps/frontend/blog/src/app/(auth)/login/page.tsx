057133a1526e635bb7a95ee7a3c7ea483ef274bb
DongYeopL <eaea7314@gmail.com>
2025-01-24 17:23:13 +0900

    [Frontend]: (update) 로그인 되어 있을 시 로그인, 회원가입 페이지 이동 불가능하게 기능 추가

--- apps/frontend/blog/src/app/(auth)/login/page.tsx
+++ apps/frontend/blog/src/app/(auth)/login/page.tsx
@@ -1,8 +1,19 @@
+"use server";
+
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
 import * as styles from "./page.css";
 
 import Login from "../_component/login/Login";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await auth();
+
+  if (session?.user) {
+    redirect("/home");
+  }
+
   return (
     <div className={styles.loginBox}>
       <Login />
--- apps/frontend/blog/src/app/(auth)/signup/page.tsx
+++ apps/frontend/blog/src/app/(auth)/signup/page.tsx
@@ -1,8 +1,19 @@
+"use server";
+
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
 import * as styles from "./page.css";
 
 import Signup from "@/app/(auth)/_component/signup/Signup";
 
-export default function SignupPage() {
+export default async function SignupPage() {
+  const session = await auth();
+
+  if (session?.user) {
+    redirect("/home");
+  }
+
   return (
     <div className={styles.signupBox}>
       <Signup />
