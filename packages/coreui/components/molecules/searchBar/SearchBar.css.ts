72861f498def64858cabd26ebbd9a7e41af8fa95
DongYeopL <eaea7314@gmail.com>
2025-02-28 17:16:03 +0900

    [Coreui]: (add) SearchBar 검색 값 삭제 버튼 추가

--- packages/coreui/components/molecules/searchBar/SearchBar.css.ts
+++ packages/coreui/components/molecules/searchBar/SearchBar.css.ts
@@ -9,9 +9,25 @@ export const searchBar = style({
 export const iconButton = style({
   display: "inline-flex",
   position: "absolute",
-  left: "10px",
   top: "50%",
+  left: "10px",
   transform: "translateY(-50%)",
+});
+
+export const closeIconButton = style({
+  display: "inline-flex",
+  position: "absolute",
+  top: "50%",
+  right: "10px",
+  transform: "translateY(-50%)",
+  opacity: 0,
+  visibility: "hidden",
+  transition: "all .1s ease-out",
+});
+
+export const active = style({
+  opacity: 1,
+  visibility: "visible",
 });
 
 export const overrideInput = style({
--- packages/coreui/components/molecules/searchBar/SearchBar.tsx
+++ packages/coreui/components/molecules/searchBar/SearchBar.tsx
@@ -7,6 +7,7 @@ import * as styles from "./SearchBar.css";
 import Input from "../../atom/input/Input";
 import IconButton from "../iconButton/IconButton";
 import { InputRef } from "../../atom/input/Input.type";
+import { composeStyles } from "@vanilla-extract/css";
 
 export interface SearchBarProps {
   /** input 창에서 엔터 및 검색 아이콘을 클릭할 때 동작할 함수 */
@@ -14,6 +15,7 @@ export interface SearchBarProps {
 }
 
 export const SearchBar = ({ onSearch }: SearchBarProps) => {
+  const [value, setValue] = useState<string>("");
   const inputRef = useRef<InputRef>(null);
 
   const handleSearch = () => {
@@ -35,12 +37,22 @@ export const SearchBar = ({ onSearch }: SearchBarProps) => {
       <Input
         ref={inputRef}
         size="m"
+        value={value}
         rounded={true}
+        onChange={(inputValue) => setValue(inputValue)}
         placeholder="검색어를 입력하세요."
         className={styles.overrideInput}
       />
       <div className={styles.iconButton}>
         <IconButton onClick={handleSearch} type="search" size="l" />
+      </div>
+      <div
+        className={composeStyles(
+          styles.closeIconButton,
+          value ? styles.active : "",
+        )}
+      >
+        <IconButton onClick={() => setValue("")} type="close" size="xs" />
       </div>
     </div>
   );
