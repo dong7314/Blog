850c8707810265c5f019dca42a2927312c34754b
DongYeopL <eaea7314@gmail.com>
2025-03-07 17:06:42 +0900

    [Frontend]: (fix) searchParams 오류 수정

--- apps/frontend/blog/src/app/posts/page.tsx
+++ apps/frontend/blog/src/app/posts/page.tsx
@@ -4,11 +4,7 @@ import Dashboard from "./_component/dashboard/Dashboard";
 import SearchDashboard from "./_component/search/SearchDashboard";
 
 type Props = {
-  searchParams: {
-    search?: string;
-    tag?: string;
-    tab?: string;
-  };
+  searchParams: Promise<{ search?: string; tag?: string; tab?: string }>;
 };
 export default async function PostPage({ searchParams }: Props) {
   const { search, tag, tab } = await searchParams;
